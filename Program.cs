4e4548e5a48929f3d2aea24862b1bf5d36429a1c
Rafael-PC <rafaelreinertd@hotmail.com>
2023-05-08 05:03:17 -0300

    Custom answers sheet

--- Program.cs
+++ Program.cs
@@ -11,19 +11,97 @@ namespace ExercicioFinalLoop
 
         static void Main(string[] args)
         {
-            char[] questionAnswers = new char[] { 'A', 'B', 'C', 'D', 'E', 'E', 'D', 'C', 'B', 'A' };
-            char[] choices = new char[] { 'A', 'B', 'C', 'D', 'E' };
+            Console.WriteLine("Gostaria de criar um gabarito personalizado?");
+            Console.WriteLine("1 - Personalizado \t 2 - Padrão");
+            Console.Write("Escolha uma opção: ");
+
+            int op;
+            ReAskCustomSheet:
+            while (!int.TryParse(Console.ReadLine(), out op))
+            {
+                Console.Write("Entrada invalida, somente numeros são aceitos\nTente novamente: ");
+            }
+            if (op < 1 || op > 2)
+            {
+                Console.Write("Escolha uma opção valida: ");
+                goto ReAskCustomSheet;
+            }
+
+            char[] questionAnswers, choices;
+            int maxChoices=1;
+            if(op == 1)
+            {
+                Console.Write("Quantas questões? ");
+
+                int nQuestions; 
+                ReAskNQuestions:
+                
+                while(!int.TryParse(Console.ReadLine(), out nQuestions)){
+                    Console.Write("Entrada invalida, somente numeros são aceitos\nTente novamente: ");
+                }
+                if (nQuestions < 1)
+                {
+                    Console.Write("Escolha uma opção valida: ");
+                    goto ReAskNQuestions;
+                }
+
+
+                questionAnswers = new char[nQuestions];
+                for(int i=0; i < questionAnswers.Length; i++)
+                {
+                    Console.Write($"Qual a Resposta correta para a questão {i+1}? ");
+                    char sheetAnswer;
+                    ReAskSheetAnswer:
+                    while (!char.TryParse(Console.ReadLine().ToUpper(), out sheetAnswer)){
+                        Console.Write("Entrada invalida, somente uma letra é aceita\nTente novamente: ");
+                    }
+                    if(!(sheetAnswer >= 'A' && sheetAnswer <= 'Z'))
+                    {
+                        Console.Write("Entrada deve ser uma letra de A a Z\nTente novamente: ");
+                        goto ReAskSheetAnswer;
+                    }
+                    questionAnswers[i] = sheetAnswer;
+                    if(maxChoices < ((int)sheetAnswer) -64) maxChoices = (int)sheetAnswer -64;
+                }
+
+                Console.WriteLine("\nQuantas escolhas por questão?");
+                Console.WriteLine($"Com base no gabarito criado as perguntas devem ter no minimo {maxChoices} escolhas!");
+
+                int nChoicesQuestions;
+                ReAskNChoicesQuestions:
+                while(!int.TryParse(Console.ReadLine(), out nChoicesQuestions))
+                {
+                    Console.Write("Entrada invalida, somente numeros são aceitos\nTente novamente: ");
+                }
+                if(!(nChoicesQuestions >= maxChoices && nChoicesQuestions <= (int) 'Z'))
+                {
+                    Console.Write($"Entrada invalida, deve ser entre {maxChoices} e {((int)'Z' - (int)'A') + 1}\nTente novamente: ");
+                    goto ReAskNChoicesQuestions;
+                }
+                choices= new char[nChoicesQuestions];
+                for (int i = 0; i < choices.Length; i++)
+                {
+                    choices[i] = (char)('A' + i);
+                }
+            }
+            else
+            {
+                questionAnswers = new char[] { 'A', 'B', 'C', 'D', 'E', 'E', 'D', 'C', 'B', 'A' };
+                choices = new char[] { 'A', 'B', 'C', 'D', 'E' };
+            }
+
             List<StudentGrade> studentsList = new List<StudentGrade>();
 
-            int op = 1;
 
+            op = 1;
             while (op == 1)
             {
-                Console.Write("Digite o nome do aluno: ");
+                Console.WriteLine("Começando Prova!");
+                Console.Write("\nDigite o nome do aluno: ");
 
             ReaskName:
                 string name = Console.ReadLine();
-                if (name.Length == 0)
+                if (name.Length == 0 || name[0] == ' ')
                 {
                     Console.Write("Nome invalido, tente novamente: ");
                     goto ReaskName;
@@ -79,7 +157,8 @@ namespace ExercicioFinalLoop
                 };
                 studentsList.Add(studentGrade);
 
-                Console.WriteLine("\n1 - Continuar \t 0 - Terminar");
+                Console.Clear();
+                Console.WriteLine("1 - Continuar \t 0 - Terminar");
                 Console.WriteLine("Selecione uma opção: ");
                 op = int.Parse(Console.ReadLine());
             }
@@ -94,7 +173,8 @@ namespace ExercicioFinalLoop
                 average += studentsList[i].grade;
             }
 
-            Console.WriteLine($"\nO aluno {studentsList[highestScoreIdx].Name} tirou a maior nota: {studentsList[highestScoreIdx].grade}");
+            Console.Clear();
+            Console.WriteLine($"O aluno {studentsList[highestScoreIdx].Name} tirou a maior nota: {studentsList[highestScoreIdx].grade}");
             Console.WriteLine($"O aluno {studentsList[lowestScoreIdx].Name} tirou a menor nota: {studentsList[lowestScoreIdx].grade}");
             Console.WriteLine("Media: " + (average / studentsList.Count));
             Console.WriteLine(studentsList.Count + " provas feitas");
